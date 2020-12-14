d8956cceabe49d834ce0c2daac0d05522ffec5b2
motemen <motemen@gmail.com>
2020-12-14 22:57:40 +0900

    Initial Commit

---
+++ BluetoothAudioDistancer/BluetoothAudioDistancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothAudioDistancerApp.swift
+//  BluetoothAudioDistancer
+//
+//  Created by motemen on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothAudioDistancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothAudioDistancer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BluetoothAudioDistancer
+//
+//  Created by motemen on 2020/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
